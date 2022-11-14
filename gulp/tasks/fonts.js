47a5b1a930eba54b8b241c5e295c9ce5732d07d3
TAlex <talex.kaliningrad@gmail.com>
2022-11-14 14:18:39 +0200

    update work fonts.js (1:23:00)

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -38,5 +38,57 @@ export const ttfToWoff = () => {
 }
 
 export const fontStyle = ()=> {
-    
+    // Файл стилей подключения шрифтов
+    let fontsFile = `${app.path.srcFolder}/scss/fonts.scss`;
+    // Проверяем существуют ли файлы шрифтов
+    fs.readdir(app.path.build.fonts, function (err, fontsFiles) {
+        if(fontsFiles) {
+            // Проверяем существует ли файл стилей для подключения шрифтов
+            if(!fs.existsSync(fontsFile)) {
+                // Если файла нет, то создаем его
+                fs.writeFile(fontsFile,'', cb);
+                let newFileOnly;
+                for(var i = 0; i < fontsFiles.length; i++) {
+                    // Записываем подключение шрифтов в файл стилей
+                    let fontFileName = fontsFiles[i].split('.')[0];
+                    if(newFileOnly !== fontFileName) {
+                        let fontName = fontFileName.split('-')[0] ? fontFileName.split('-')[0] : fontFileName;
+                        let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[1] : fontFileName;
+                        if(fontWeight.toLowerCase() === 'thin') {
+                            fontWeight = 100;
+                        } else if(fontWeight.toLowerCase() === 'extralight') {
+                            fontWeight = 200;
+                        } else if(fontWeight.toLowerCase() === 'light') {
+                            fontWeight = 300;
+                        } else if(fontWeight.toLowerCase() === 'medium') {
+                            fontWeight = 500;
+                        } else if(fontWeight.toLowerCase() === 'semibold') {
+                            fontWeight = 600;
+                        } else if(fontWeight.toLowerCase() === 'bold') {
+                            fontWeight = 700;
+                        } else if(fontWeight.toLowerCase() === 'extrabold' || fontWeight.toLowerCase() === 'heavy') {
+                            fontWeight = 800;
+                        } else if(fontWeight.toLowerCase() === 'black') {
+                            fontWeight = 900;
+                        } else {
+                            fontWeight = 400;
+                        }
+                        fs.appendFile(fontsFile,
+                            `@font-face{
+                                font-family: ${fontName};
+                                font-display: swap;
+                                src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");
+                                font-weight: ${fontWeight};
+                                font-style: normal;\n}\r\n`, cb);
+                        newFileOnly = fontFileName;
+                    }
+                }
+            } else {
+                //Если файл есть, выводим сообщение
+                console.log("Файл scss/fonts.scss уже существует. Для обновления файла нужно его удалить!");
+            }
+        }
+    });
+    return app.gulp.src(`${app.path.srcFolder}`);
+    function cb() { }
 }

402a6bca8fda21dfcc4b715a1a4d47355d0c38e5
TAlex <talex.kaliningrad@gmail.com>
2022-11-18 14:09:11 +0200

    start header with video

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -11,23 +11,28 @@ export const path = {
         html: `${buildFolder}/`,
         images: `${buildFolder}/img/`,
         fonts: `${buildFolder}/fonts/`,
-        files: `${buildFolder}/files/`
+        files: `${buildFolder}/files/`,
+        video: `${buildFolder}/video/`
     },
     src: {
         js: `${srcFolder}/js/app.js`,
         images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
         svg: `${srcFolder}/img/**/*.svg`,
-        scss: `${srcFolder}/scss/style.scss`,
+        css: `${srcFolder}/css/*.css`,
+        scss: `${srcFolder}/scss/*.scss`,
         html: `${srcFolder}/*.pug`,
         files: `${srcFolder}/files/**/*.*`,
-        svgicons:`${srcFolder}/svgicons/*.svg`
+        svgicons:`${srcFolder}/svgicons/*.svg`,
+        video: `${srcFolder}/video/**/*.*`
     },
     watch:{
         js: `${srcFolder}/js/**/*.js`,
         scss: `${srcFolder}/scss/**/*.scss`,
+        css: `${srcFolder}/css/**/*.css`,
         html: `${srcFolder}/**/*.pug`,
         images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg,ico}`,
-        files: `${srcFolder}/files/**/*.*`
+        files: `${srcFolder}/files/**/*.*`,
+        video: `${srcFolder}/video/**/*.*`
     },
     clean: buildFolder,
     buildFolder: buildFolder,
--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,4 +1,8 @@
 export const copy = () => {
     return app.gulp.src(app.path.src.files)
         .pipe(app.gulp.dest(app.path.build.files))
+        .pipe(app.gulp.src(app.path.src.css))
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(app.gulp.src(app.path.src.video))
+        .pipe(app.gulp.dest(app.path.build.video))
 }
